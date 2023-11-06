87741fdf7988cff8cc568ee02bc38a5cabaf187f
jpedroco <30795975+jpedroco@users.noreply.github.com>
2023-11-06 17:06:15 +0100

    .

---
+++ MAUIAndroidFS/MiUdp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClasesGenerales;
+
+namespace MAUIAndroidFS
+{
+    public class MiUdp
+    {
+        public static void Envia(string mensaje)
+        {
+            UdpCommunication.Envia("192.168.1.11", 9394, mensaje);
+        }
+    }
+}
